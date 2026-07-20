63e7143d57ed2632391bbc089d031fa1eee4b308
milesmorgan <miles.morgan@bsu.edu>
2026-07-20 00:56:06 -0400

    added min and exact stat options

--- src/statsearch.py
+++ src/statsearch.py
@@ -5,3 +5,19 @@ def search_min(pokemondata,stat, min):
             if pokemondata[i]["forms"][f]["stats"][stat] >= min:
                 results.append(f.title())
     return results
+
+def search_max(pokemondata,stat, min):
+    results = []
+    for i in pokemondata:
+        for f in pokemondata[i]["forms"]:
+            if pokemondata[i]["forms"][f]["stats"][stat] <= min:
+                results.append(f.title())
+    return results
+
+def search_exact(pokemondata,stat, min):
+    results = []
+    for i in pokemondata:
+        for f in pokemondata[i]["forms"]:
+            if pokemondata[i]["forms"][f]["stats"][stat] == min:
+                results.append(f.title())
+    return results
