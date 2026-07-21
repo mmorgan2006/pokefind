469bfe65020ddadd075903185369e8c59dd3b9a8
mmorgan2006 <miles.morgan@bsu.edu>
2026-07-21 01:48:11 -0400

    Added the ability to add moves into the search field

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import copy
 
 ALL_POKEMON,ALL_MOVES,ALL_ABILITIES = load.load_data()
 ABILITY_NAMES = []
+MOVE_NAMES = []
 TYPELIST = [
     "Normal", "Fire", "Water", "Grass", "Electric", "Ice",
     "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug",
@@ -14,6 +15,8 @@ TYPELIST = [
 
 for i in ALL_ABILITIES:
     ABILITY_NAMES.append(ALL_ABILITIES[i]["name"])
+for i in ALL_MOVES:
+    MOVE_NAMES.append(ALL_MOVES[i]["name"])
 class SearchTab(qt.QWidget):
     def __init__(self):
         super().__init__()
@@ -26,8 +29,10 @@ class SearchTab(qt.QWidget):
         for stat_name in ["HP", "Attack", "Defense", "Special-Attack", "Special-Defense", "Speed"]:
             stat_value = qt.QLineEdit()
             stat_value.setMaxLength(3)
+            stat_value.setFixedWidth(40)
             mode_choice = qt.QComboBox()
             mode_choice.addItems(["Minimum","Exact","Maximum"])
+            mode_choice.setFixedWidth(130)
 
             self.stats[stat_name] = {"value": stat_value, "mode": mode_choice}
 
@@ -51,8 +56,10 @@ class SearchTab(qt.QWidget):
         self.ability_completer = qt.QCompleter(ABILITY_NAMES)
         self.ability_completer.setCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
         self.ability.setCompleter(self.ability_completer)
+        left.addLayout(details)
 
         # TYPES
+        details = qt.QHBoxLayout()
         self.type1 = qt.QLineEdit()
         self.type1.setPlaceholderText("Type 1")
         self.type2 = qt.QLineEdit()
@@ -67,13 +74,25 @@ class SearchTab(qt.QWidget):
 
         # MOVES
         details = qt.QHBoxLayout()
-        text = qt.QLabel("Moves")
-        text.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        details.addWidget(text)
-        details.addWidget(qt.QPushButton("+ Move"))
-        details.addWidget(qt.QPushButton("Clear Moves"))
+
+        move_completer = qt.QCompleter(MOVE_NAMES)
+        move_completer.setCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
+        self.move_field = qt.QLineEdit()
+        self.move_field.setPlaceholderText("Add Move")
+        self.move_field.setCompleter(move_completer)
+        details.addWidget(self.move_field)
+
+        self.add_move = qt.QPushButton("+")
+        self.add_move.setFixedWidth(30)
+        details.addWidget(self.add_move)
+
+        self.clear_moves = qt.QPushButton("Clear Moves")
+        details.addWidget(self.clear_moves)
+
         left.addLayout(details)
         self.moves = qt.QListWidget()
+        self.moves.setFixedHeight(125)
+        self.moves_list = []
 
         left.addWidget(self.moves)
 
@@ -83,6 +102,7 @@ class SearchTab(qt.QWidget):
 
         right.addWidget(qt.QLabel("Results"))
         self.results_list = qt.QListWidget()
+        self.results_list.setFixedWidth(200)
         right.addWidget(self.results_list)
 
         FullLayout = qt.QHBoxLayout()
@@ -91,8 +111,9 @@ class SearchTab(qt.QWidget):
 
         self.setLayout(FullLayout)
         self.searchbutton.clicked.connect(self.Search)
-
-
+        self.add_move.clicked.connect(self.AddMove)
+        self.results_list.itemDoubleClicked.connect(self.ClickResult)
+        self.moves.itemClicked.connect(self.ClickMove)
     def Search(self):
         if self.ability.text() in ABILITY_NAMES:
             validpokemon = search.search_ability(self.ability.text(),ALL_POKEMON)
@@ -118,6 +139,21 @@ class SearchTab(qt.QWidget):
         for i in results:
             self.results_list.addItem(i)
 
+    def AddMove(self):
+        move = self.move_field.text()
+        self.move_field.setText("")
+        if len(self.moves_list) < 4:
+            if move in MOVE_NAMES and move not in self.moves_list:
+                self.moves_list.append(move)
+                self.moves.addItem(move)
+
+    def ClickResult(self,item):
+        print(item.text())
+
+    def ClickMove(self, item):
+        row = self.moves.row(item)
+        self.moves.takeItem(row)
+        self.moves_list.remove(item.text())
 class OtherTab(qt.QWidget):
     def __init__ (self):
         super().__init__()
@@ -136,8 +172,8 @@ class MainWindow(qt.QMainWindow):
 
         self.setCentralWidget(tabs)
         self.setWindowTitle("PokéFind")
-        self.setMinimumHeight(600)
-        self.setMinimumWidth(800)
+        self.setMinimumHeight(300)
+        self.setMinimumWidth(300)
 
 if __name__ == "__main__":
     app = qt.QApplication()
